a4c4f9ff375a2ceaca263631ecc96be8b9c1860d
Vasiliy Kvasov <hello@vasiliykvasov.ru>
2023-08-17 01:33:12 +0300

    something works need to refactor

--- script.js
+++ script.js
@@ -96,7 +96,11 @@ let fees = [
         }
     ];
 
+/* --- */
+
 /* Расчеты */
+function retailPrice() {
+
 let sumCosts = 0; // Сумма расходов и прибыли
 let sumCommissions = 0; // Сумма комиссий маркетплейса
 let sumFees = 0; // Сумма тарифов маркетплейса
@@ -105,10 +109,11 @@ let sumFees = 0; // Сумма тарифов маркетплейса
 /* В sumCosts суммируются все значения value из расходов и прибылей costs. */
 console.log("- Расходы и прибыль:")
 costs.forEach(function(cost) {
-    sumCosts = sumCosts + cost["value"];
-    console.log(cost["name"] + ": " + cost["value"]);
+    sumCosts = sumCosts + Number(cost["value"]);
+    console.log("name: " + cost["name"] + ", value: " + cost["value"]);
+    console.log("sumCosts: " + sumCosts)
 })
-console.log(sumCosts);
+console.log("sumCosts (final): " + sumCosts);
 
 /* Сумма комиссий */
 /* В sumCommissions суммируются все значения percent из комиссий commissions.
@@ -141,3 +146,86 @@ console.log("- Оптовая цена: " + wholesalePrice);
 let result = (wholesalePrice + sumCosts + sumFees) / (1 - sumCommissions / 100);
 result = result.toFixed(2);
 console.log("- Результат: " + result);
+}
+
+
+retailPrice();
+
+
+/* --- */
+
+/* Вывод оптовой цены */
+const inputWholesalePrice = document.querySelector(".wholesale-price__number");
+inputWholesalePrice.value = wholesalePrice;
+
+/* Добавить расход */
+const divCosts = document.querySelector(".costs");
+const buttonAddCost = document.querySelector(".add__cost");
+buttonAddCost.addEventListener('click', function () {
+    addCost();
+});
+
+function addCost() {
+    /* Добавить название */
+    const addCostInputGroup = document.createElement("div");
+    addCostInputGroup.classList.add("cost", "input-group", "mb-3");
+    divCosts.append(addCostInputGroup);
+    const addCostFormFloating = document.createElement("div");
+    addCostFormFloating.classList.add("form-floating");
+    addCostInputGroup.append(addCostFormFloating);
+    const addCostName = document.createElement("input");
+    addCostName.classList.add("new-cost__name", "form-control");
+    const addCostNameLabel = document.createElement("label");
+    addCostNameLabel.innerText = "Название";
+    addCostFormFloating.append(addCostName, addCostNameLabel);
+    const addCostButton = document.createElement("button");
+    addCostButton.classList.add("new-cost__button", "btn", "btn-primary");
+    addCostButton.setAttribute("type", "button");
+    addCostButton.setAttribute("aria-label", "Сохранить");
+    addCostButton.innerHTML = '<i class="bi bi-check"></i>';
+    addCostInputGroup.append(addCostButton);
+    
+    /* Добавить ввод расходов */
+    const addCostFixedInputGroup = document.createElement("div");
+    addCostFixedInputGroup.classList.add("fixed-cost", "input-group", "mb-3");
+    const addCostFixedFormFloating = document.createElement("div");
+    addCostFixedFormFloating.classList.add("form-floating");
+    const addCostFixedInput = document.createElement("input");
+    addCostFixedInput.classList.add("fixed-cost__input", "form-control");
+    addCostFixedInput.setAttribute("type", "number");
+    addCostFixedInput.setAttribute("min", "0");
+    addCostFixedInput.setAttribute("step", "0.01");
+    const addCostFixedLabel = document.createElement("label");
+
+    addCostButton.addEventListener('click', function () {
+        let costName = addCostName.value;
+        console.log(costName.length);
+        if (costName.length === 0) {
+            alert("Добавить ошибку в валидации из-за пустого имени");
+        } else {
+            addCostInputGroup.after(addCostFixedInputGroup); //divCosts.append(addCostFixedInputGroup);
+            addCostFixedInputGroup.append(addCostFixedFormFloating);
+            //addCostFixedInput.setAttribute("placeholder", "Введите " + costName);
+            addCostFixedInput.setAttribute("aria-label", costName);
+            addCostFixedLabel.innerText = costName;
+            addCostFixedFormFloating.append(addCostFixedInput, addCostFixedLabel);
+
+            costs.push({name:costName});
+            addCostFixedInput.addEventListener('change', function () {
+                costName = addCostFixedInput.ariaLabel;
+                console.log("change: " + costName);
+                let addCostIndex = costs.find(findCost => findCost.name === costName);
+                addCostIndex["value"] = addCostFixedInput.value;
+                console.log(addCostIndex);
+                console.log(costs);
+                retailPrice();
+            });
+            removeName(addCostInputGroup);
+        }
+    })
+};
+
+function removeName(element) {
+element.remove();
+};
+
