9aea95809c7a2de6175c64febc687b22c07a1322
Vasiliy Kvasov <hello@vasiliykvasov.ru>
2024-02-06 00:39:50 +0300

    error?

--- js/objects/commission.js
+++ js/objects/commission.js
@@ -115,7 +115,7 @@ export class Commission extends Expenditure {
         this.showButtonDelete();
 
         // Вывести смену типа
-        this.showChangeType('commission');
+        this.showChangeType('commission'); // Комиссия
 
         // Вывести ввод имени
         this.showName();
